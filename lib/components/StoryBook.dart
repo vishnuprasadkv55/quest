bd5160c353ed577aae4f484e4395660415954990
vishnu <vishnuprasadkv55@gmail.com>
2020-07-25 17:05:48 +0530

    book widget

--- lib/components/StoryBook.dart
+++ lib/components/StoryBook.dart
@@ -6,9 +6,10 @@ class StoryBook extends StatefulWidget {
   @override
   _StoryBookState createState() => _StoryBookState();
   final contests;
-  StoryBook({this.contests});
+  var currentPage;
+  StoryBook({this.contests}) :
+  currentPage = (contests.map((item) => item.generalConfig.thumbnail).toList()).length - 1.0;
 }
-
 var cardAspectRatio = 12.0 / 16.0;
 var widgetAspectRatio = cardAspectRatio * 1.2;
 
@@ -17,17 +18,16 @@ class _StoryBookState extends State<StoryBook> {
   Widget build(BuildContext context) {
     List images = widget.contests.map((item) => item.generalConfig.thumbnail).toList();
     List title = widget.contests.map((item) => item.gameLabel).toList();
-    var currentPage = images.length - 1.0;
     PageController controller = PageController(initialPage: images.length - 1);
     controller.addListener(() {
       setState(() {
-        currentPage = controller.page;
+        widget.currentPage = controller.page;
       });
     });
     return Stack(
       children: <Widget>[
         CardScrollWidget(
-          currentPage: currentPage,
+          currentPage: widget.currentPage,
           images: images,
           title: title,
         ),
